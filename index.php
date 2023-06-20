9da33dba55800af8b06e0eff430e5e7756ca2e08
Sergio Justicia <quakyman15@gmail.com>
2023-06-20 12:21:13 +0200

    Añadido etiquetas index, links, tamaño fuente y script

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pruebas</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <script src="script/script.js"></script>
+</head>
+<body>
+  
+</body>
+
+
+
+
 <?php 
   echo "Hola Mundo";
   echo "<br> Ésto es una prueba";
+
+?>
+  </html>
--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 function alert1() {
 
-alert("Creating scripts")
+alert("Creating scripts");
+var script = document.createElement('script');  // Create a <script> element
 
 }
